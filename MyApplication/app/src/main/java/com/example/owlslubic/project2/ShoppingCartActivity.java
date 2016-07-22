ec87e8ec206d2dc80a76127f96b96f3a2d5999a2
Alice Lubic <lubicalice@gmail.com>
2016-07-21 21:01:59 -0400

    started xml layout

---
+++ MyApplication/app/src/main/java/com/example/owlslubic/project2/ShoppingCartActivity.java
@@ -0,0 +1,14 @@
+package com.example.owlslubic.project2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShoppingCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+
+    }
+}
