712ae5c486c97084799a99e4f3630505f1c6f89a
Alice Lubic <lubicalice@gmail.com>
2016-07-29 11:14:26 -0400

    quantity and price change work, total does not, workin on wonky dialog now

--- MyApplication/app/src/main/java/com/example/owlslubic/project2/ShoppingCartRvAdapter.java
+++ MyApplication/app/src/main/java/com/example/owlslubic/project2/ShoppingCartRvAdapter.java
@@ -23,6 +23,8 @@ public class ShoppingCartRvAdapter extends RecyclerView.Adapter<ShoppingCartView
     Context mContext;
     List<CartObject> cartList = new ArrayList<>();
     DatabaseHelper helper = DatabaseHelper.getInstance(mContext);
+    double total;
+
 
 
     //constructor - adding list parameter
@@ -37,6 +39,8 @@ public class ShoppingCartRvAdapter extends RecyclerView.Adapter<ShoppingCartView
     public ShoppingCartViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_shopping_cart, parent, false);
         ShoppingCartViewHolder viewHolder = new ShoppingCartViewHolder(view);
+
+
         return viewHolder;
     }
 
@@ -46,13 +50,13 @@ public class ShoppingCartRvAdapter extends RecyclerView.Adapter<ShoppingCartView
     public void onBindViewHolder(final ShoppingCartViewHolder holder, final int position) {
         final CartObject item = cartList.get(position);
         holder.mName.setText(item.getmName());
-        holder.mPrice.setText("$"+item.getmPrice());
+        holder.mPrice.setText("$" + item.getmPrice());
         holder.mQuantity.setText(String.valueOf(item.getmQuantity()));
         holder.mPlantImage.setImageResource(item.getmImage());
 
         //  Log.d(KEY, cartList.size()+ "this is the size of the cartlist upon being bound to the shopping cart");
 
-
+        total += helper.getQuantityFromTable(item) * item.getmPrice();
         holder.mRemove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,40 +64,40 @@ public class ShoppingCartRvAdapter extends RecyclerView.Adapter<ShoppingCartView
                 removeByPosition(holder.getAdapterPosition());
 
                 //   Log.d(KEY, "removeItemByPosition removed item from recyclerview from sc adapter");
-                Toast.makeText(mContext, "Deleted from cart!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, "Adios, " + item.getmName() + "!", Toast.LENGTH_SHORT).show();
             }
         });
         holder.mIncrement.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //quantity db and textview, price textview, and total textview should reflect this
-
-                holder.mPrice.setText("$" + getNewPrice(item, holder));
-//                holder.mTotal.setText(String.valueOf());
+                total += helper.getQuantityFromTable(item) * item.getmPrice();
                 helper.increaseQty(item);
                 holder.mQuantity.setText(String.valueOf(String.valueOf(helper.getQuantityFromTable(item))));
+                holder.mPrice.setText("$" + getNewPrice(item, holder));
+//                holder.mTotal.setText("$"+total);
+
             }
         });
         holder.mDecrement.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                holder.mPrice.setText("$" + getNewPrice(item, holder));
-//                holder.mTotal.setText(String.valueOf());
+
                 helper.decreaseQty(item);
                 holder.mQuantity.setText(String.valueOf(helper.getQuantityFromTable(item)));
+                holder.mPrice.setText("$" + getNewPrice(item, holder));
+//                holder.mTotal.setText("$"+total);
+         //       total = total + holder.mPrice.getText();
+
             }
         });
 
     }
 
+    //method that takes whatever is in the setTexts and adds them...
+
     @Override
     public int getItemCount() {
         return cartList.size();
-    }
-
-    public void removeItem(ShoppingCartViewHolder holder, CartObject item){
-        int position = holder.getAdapterPosition();
-        removeByPosition(position);
     }
 
     public void removeByPosition(int position) {
@@ -104,38 +108,31 @@ public class ShoppingCartRvAdapter extends RecyclerView.Adapter<ShoppingCartView
     }
 
 
-    //ok all of these methods need some sort of counter to keep track of what they are adding to or nah
-
-
     //method to affect price:
     public double getNewPrice(CartObject item, ShoppingCartViewHolder holder) {
+        double price = item.getmPrice();
         double newPrice;
         int qty = helper.getQuantityFromTable(item);
         int position = holder.getAdapterPosition();
-        if (qty <= 0) {
+        if (qty > 0) {
+            if (qty == 1) {
+                return price;
+            }
+            newPrice = price*qty;
+            newPrice = Math.floor(newPrice * 100) / 100;
+         //   total += helper.getQuantityFromTable(item) * item.getmPrice();
+            return newPrice;
+
+        } else {
             removeByPosition(position);
-            Log.d(KEY,"quantity hit 0, removed "+item.getmName());
+            Toast.makeText(mContext, "Adios, " + item.getmName() + "!", Toast.LENGTH_SHORT).show();
+            Log.d(KEY, "quantity hit 0, removed " + item.getmName());
+         //   total += helper.getQuantityFromTable(item) * item.getmPrice();
             return 0;
         }
-        newPrice = item.getmPrice() * qty;
-        newPrice = Math.floor(newPrice * 100) / 100;
-            return newPrice;
+
     }
 
 
-    //method to affect total:
-    public double removeFromPrice(double price) {
-        double newPrice = price - price;
-        return newPrice;
-    }
+
 }
-
-//    //need some sort of counter to keep track
-//    public double updateTotal(double price){
-//        int newTotal = oldTotal + price;
-//        return newTotal;
-//    }
-//
-
-
-
